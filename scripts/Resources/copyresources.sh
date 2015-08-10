14ef1f13eaa039637d41591c5132cdd550cf5320
dark_sylinc <dark_sylinc@yahoo.com.ar>
2015-08-10 16:17:00 +0000

    README.md edited online with Bitbucket

---
+++ scripts/Resources/copyresources.sh
@@ -0,0 +1,27 @@
+mkdir ../../bin/Release_Linux/Resources
+mkdir ../../bin/Release_Linux/Resources/Fonts
+mkdir ../../bin/Release_Linux/Resources/Models
+mkdir ../../bin/Release_Linux/Resources/Icons
+mkdir ../../bin/Release_Linux/Resources/Icons/32x32
+mkdir ../../bin/Release_Linux/Resources/RTShaderLib
+
+cp -r Icons/32x32 ../../bin/Release_Linux/Resources/Icons
+
+cp -r Fonts/* ../../bin/Release_Linux/Resources/Fonts
+cp -r Other/* ../../bin/Release_Linux/Resources/Models
+cp -r RTShaderLib/* ../../bin/Release_Linux/Resources/RTShaderLib
+
+cp Blender/Axis.material ../../bin/Release_Linux/Resources/Models
+cp Blender/Axis.mesh ../../bin/Release_Linux/Resources/Models
+
+cp Blender/Bones/BoneMesh.material ../../bin/Release_Linux/Resources/Models
+
+cp Blender/Bones/Bones.png ../../bin/Release_Linux/Resources/Models
+
+cp Blender/Bones/BoneTip.mesh ../../bin/Release_Linux/Resources/Models
+
+cp Blender/Bones/BoneGlobe.mesh ../../bin/Release_Linux/Resources/Models
+
+mkdir ../../bin/Release_Linux/Plugins
+cp ../../bin/Release/Readme.txt ../../bin/Release_Linux/
+cp ../../bin/Release/ChangeLog.txt ../../bin/Release_Linux/
